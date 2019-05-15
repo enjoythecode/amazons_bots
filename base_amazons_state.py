a9036bcb86b8cfb4b7a3e5fa2ae29592086b8395
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2019-05-15 08:40:16 +0300

    fix code style even more

--- base_amazons_state.py
+++ base_amazons_state.py
@@ -29,6 +29,7 @@ class AmazonsState:
         By convention the players are numbered 1 and 2.
         Assumes square game board
     """
+
     def __init__(self, board, pjm=2):
         self.playerJustMoved = pjm  # At the root pretend the player just moved is player 2 - player 1 moves first
         self.board = copy.deepcopy(board)
@@ -59,7 +60,7 @@ class AmazonsState:
         queen_moves = self.get_possible_queen_moves(player)
 
         for q in queen_moves:
-            out += self.count_possible_shots_from_queen(q[1],q[0])
+            out += self.count_possible_shots_from_queen(q[1], q[0])
 
         return out
 
@@ -72,7 +73,7 @@ class AmazonsState:
 
         queen_moves = self.get_possible_queen_moves(player)
         for q in queen_moves:
-            out.extend([ [q[0],q[1],s] for s in self.get_possible_shots_from_queen(q[1],q[0]) ])
+            out.extend([[q[0], q[1], s] for s in self.get_possible_shots_from_queen(q[1], q[0])])
         return out
 
     def get_possible_queen_moves(self, player=None):
@@ -82,8 +83,8 @@ class AmazonsState:
         for q_x in range(self.game_size):
             for q_y in range(self.game_size):
                 if self.board[q_x][q_y] == player:
-                    q = str(q_x)+str(q_y)
-                    out.extend( [[q, x] for x in self.get_valid_moves(q)])
+                    q = str(q_x) + str(q_y)
+                    out.extend([[q, x] for x in self.get_valid_moves(q)])
 
         return out
 
@@ -95,7 +96,7 @@ class AmazonsState:
         for q_x in range(self.game_size):
             for q_y in range(self.game_size):
                 if self.board[q_x][q_y] == player:
-                    q = str(q_x)+str(q_y)
+                    q = str(q_x) + str(q_y)
                     out += self.count_valid_moves(q)
 
         return out
@@ -110,11 +111,11 @@ class AmazonsState:
         out = []
         from_x = int(cell_from[0])
         from_y = int(cell_from[1])
-        ignore_x = int(ignore[0]) if not ignore is None else -1
-        ignore_y = int(ignore[1]) if not ignore is None else -1
+        ignore_x = int(ignore[0]) if ignore is not None else -1
+        ignore_y = int(ignore[1]) if ignore is not None else -1
 
         x, y = from_x, from_y
-        while x+1 < self.game_size:
+        while x + 1 < self.game_size:
             x += 1
             if self.board[x][y] != 0:
                 if ignore_x == x and ignore_y == y:
@@ -140,10 +141,10 @@ class AmazonsState:
                 out.append(str(x) + str(y))
 
         x, y = from_x, from_y
-        while y+1 < self.game_size:
+        while y + 1 < self.game_size:
             y += 1
             if self.board[x][y] != 0:
-                if (ignore_x == x and ignore_y == y):
+                if ignore_x == x and ignore_y == y:
                     if include_ignore:
                         out.append(str(x) + str(y))
                     continue
@@ -166,11 +167,11 @@ class AmazonsState:
                 out.append(str(x) + str(y))
 
         x, y = from_x, from_y
-        while x+1 < self.game_size and y+1 < self.game_size:
+        while x + 1 < self.game_size and y + 1 < self.game_size:
             x += 1
             y += 1
             if self.board[x][y] != 0:
-                if (ignore_x == x and ignore_y == y):
+                if ignore_x == x and ignore_y == y:
                     if include_ignore:
                         out.append(str(x) + str(y))
                     continue
@@ -180,11 +181,11 @@ class AmazonsState:
                 out.append(str(x) + str(y))
 
         x, y = from_x, from_y
-        while x+1 < self.game_size and y > 0:
+        while x + 1 < self.game_size and y > 0:
             x += 1
             y -= 1
             if self.board[x][y] != 0:
-                if (ignore_x == x and ignore_y == y):
+                if ignore_x == x and ignore_y == y:
                     if include_ignore:
                         out.append(str(x) + str(y))
                     continue
@@ -194,11 +195,11 @@ class AmazonsState:
                 out.append(str(x) + str(y))
 
         x, y = from_x, from_y
-        while x > 0 and y+1 < self.game_size:
+        while x > 0 and y + 1 < self.game_size:
             x -= 1
             y += 1
             if self.board[x][y] != 0:
-                if (ignore_x == x and ignore_y == y):
+                if ignore_x == x and ignore_y == y:
                     if include_ignore:
                         out.append(str(x) + str(y))
                     continue
@@ -212,7 +213,7 @@ class AmazonsState:
             x -= 1
             y -= 1
             if self.board[x][y] != 0:
-                if (ignore_x == x and ignore_y == y):
+                if ignore_x == x and ignore_y == y:
                     if include_ignore:
                         out.append(str(x) + str(y))
                     continue
@@ -231,7 +232,7 @@ class AmazonsState:
         ignore_y = int(ignore[1]) if ignore is not None else -1
 
         x, y = from_x, from_y
-        while x+1 < self.game_size:
+        while x + 1 < self.game_size:
             x += 1
             if self.board[x][y] != 0:
                 if ignore_x == x and ignore_y == y:
@@ -257,7 +258,7 @@ class AmazonsState:
                 out += 1
 
         x, y = from_x, from_y
-        while y+1 < self.game_size:
+        while y + 1 < self.game_size:
             y += 1
             if self.board[x][y] != 0:
                 if ignore_x == x and ignore_y == y:
@@ -283,7 +284,7 @@ class AmazonsState:
                 out += 1
 
         x, y = from_x, from_y
-        while x+1 < self.game_size and y+1 < self.game_size:
+        while x + 1 < self.game_size and y + 1 < self.game_size:
             x += 1
             y += 1
             if self.board[x][y] != 0:
@@ -297,7 +298,7 @@ class AmazonsState:
                 out += 1
 
         x, y = from_x, from_y
-        while x+1 < self.game_size and y > 0:
+        while x + 1 < self.game_size and y > 0:
             x += 1
             y -= 1
             if self.board[x][y] != 0:
@@ -311,7 +312,7 @@ class AmazonsState:
                 out += 1
 
         x, y = from_x, from_y
-        while x > 0 and y+1 < self.game_size:
+        while x > 0 and y + 1 < self.game_size:
             x -= 1
             y += 1
             if self.board[x][y] != 0:
@@ -347,7 +348,7 @@ class AmazonsState:
         p1 = self.count_possible_queen_moves(1)
         p2 = self.count_possible_queen_moves(2)
         if p1 == 0 and p2 == 0:
-            return self.playerJustMoved # player who just moved wins
+            return self.playerJustMoved  # player who just moved wins
         elif p1 == 0:
             return 2  # player 2 won
         elif p2 == 0:
@@ -360,4 +361,4 @@ class AmazonsState:
         """
         return "\n".join(
             [" ".join([prettify_board_character(c) for c in x]) for x in self.board]
-            )
+        )
